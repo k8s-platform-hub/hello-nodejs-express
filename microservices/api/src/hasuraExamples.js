efe698a5a1be712e6ef36d9c2d58e9aac081cc29
Praveen D <praveen@hasura.io>
2018-02-15 16:53:08 +0530

    deploy

--- microservices/api/src/hasuraExamples.js
+++ microservices/api/src/hasuraExamples.js
@@ -9,7 +9,7 @@ router.route("/").get(function (req, res) {
 })
 */
 router.route("/").get(function (req, res) {
-  res.render("index.html");
+  res.render("index");
 })
 
 router.route("/examples/data").get(function (req, res) {

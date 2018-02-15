b1768a7d5d3aa9f83b21ac2339d16fafce62e84d
Praveen D <praveen@hasura.io>
2018-02-15 19:11:50 +0530

    deploy

--- microservices/api/src/hasuraExamples.js
+++ microservices/api/src/hasuraExamples.js
@@ -20,7 +20,7 @@ router.route("/examples/data").get(function (req, res) {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'X-Hasura-User-Id': 0,
+      'X-Hasura-User-Id': '0',
       'X-Hasura-Role': 'anonymous'
     },
     body: JSON.stringify({
@@ -62,7 +62,51 @@ router.route("/examples/auth").get(function (req, res) {
     res.render("auth_user", {'base_domain': baseDomain, 'user_id': req.headers['x-hasura-user-id'], 
       'roles': req.headers['x-hasura-allowed-roles']});
   }
+})
 
+router.route("/examples/filestore").get(function (req, res) {
+  const baseDomain = req.headers['x-hasura-base-domain'];
+  // check if logged in user or not
+  if (req.headers['x-hasura-allowed-roles'].includes("anonymous")) {
+    res.render("filestore_anonymous", {'base_domain': baseDomain});
+  } else {
+    console.log(req.headers);
+
+      requestPayload = {
+            "type": "select",
+            "args": {
+                "table": {
+                    "name": "hf_file",
+                    "schema": "hf_catalog"
+                },
+                "columns": [ "*" ],
+                "where": {"user_id": req.headers['x-hasura-user-id']}
+            }
+      }
+  var selectOptions = {
+    url: config.projectConfig.url.data,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Hasura-User-Id': '1',
+      'X-Hasura-Role': 'admin'
+    },
+    body: JSON.stringify(requestPayload)
+  };
+  request(selectOptions, function(error, response, body) {
+    if (error) {
+        console.log('Error from select request: ');
+        console.log(error)
+        res.status(500).json({
+          'error': error,
+          'message': 'Select request failed'
+        });
+    }
+    res.render("filestore_user", {'base_domain': baseDomain, 'files': response});
+  })
+
+
+  }
 })
 
 module.exports = router;
